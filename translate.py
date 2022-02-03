5917f40e31b4dc3d96b616d0cd57da5f9a9d579f
Paulo Alves <pauloroberto.21s@gmail.com>
2022-02-03 18:36:17 -0300

    Removendo números mágicos

--- translate.py
+++ translate.py
@@ -4,9 +4,11 @@ from time import sleep
 import readline
 import pandas as pd
 
-# número de itens a serem lidos
-# interessante não fazer mais do que trinta, no máximo. Mas idealmente, cerca de 15.
-#REQUEST_NUMBER=int(input("Insira o número de palavras a buscar: "))
+# Variáveis numéricas
+TRANSLATION_NUMBER = 3
+EXAMPLE_NUMBER = TRANSLATION_NUMBER
+ITERATION_CYCLE=20
+WAIT_TIME=30
 REQUEST_NUMBER=30
 
 # filename autocompletion
@@ -28,14 +30,14 @@ if __name__ == "__main__":
     iterations = 0
     for cell in head.iteritems():
         for word in cell[1]:
-            if iterations == 20:
-                for i in range(21): 
-                    print("waiting... {}/20".format(i))
+            if iterations == ITERATION_CYCLE:
+                for i in range(1, WAIT_TIME+1): 
+                    print("waiting... {}/{}".format(i, WAIT_TIME))
                     sleep(1)
             
             iterations += 1
             print(word)
-            word_data = get_word_data(word, 3, 3)
+            word_data = get_word_data(word, TRANSLATION_NUMBER, EXAMPLE_NUMBER)
             if word_data == False: # retorna falso quando não há exemplo objetivo de tradução (mesmo havendo exemplos em frases; isso é feito por medida de segurança)
                 continue
             df = produce_dataframe(word_data)
